20a340e783ad08b1286ee1289ce2ca97baa40fdc
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-11-08 23:05:11 +0300

    ArrayLists and adapter added

--- app/src/main/java/com/asimodabas/movie_detail_app/KategoriAdapter.kt
+++ app/src/main/java/com/asimodabas/movie_detail_app/KategoriAdapter.kt
@@ -1,14 +1,49 @@
 package com.asimodabas.movie_detail_app
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 
-class KategoriAdapter(private val mContext:Context,private val kategoriListe:List<Kategoriler>) {
+class KategoriAdapter(private val mContext: Context, private val kategoriListe: List<Kategoriler>) :
+    RecyclerView.Adapter<KategoriAdapter.CardTasarimTutucu>() {
 
-inner class CardTasarimTutucu(tasarim:View) : RecyclerView.ViewHolder(tasarim){
+    inner class CardTasarimTutucu(tasarim: View) : RecyclerView.ViewHolder(tasarim) {
+
+        var kategori_card: CardView
+        var textViewKategoriAd: TextView
+
+        init {
+            kategori_card = tasarim.findViewById(R.id.kategori_card)
+            textViewKategoriAd = tasarim.findViewById(R.id.textViewKategoriAd)
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardTasarimTutucu {
+        val tasarim =
+            LayoutInflater.from(mContext).inflate(R.layout.kategori_card_tasarim, parent, false)
+        return CardTasarimTutucu(tasarim)
+    }
+
+    override fun onBindViewHolder(holder: CardTasarimTutucu, position: Int) {
+
+        val kategori = kategoriListe.get(position)
+holder.textViewKategoriAd.text = kategori.kategori_ad
+        holder.kategori_card.setOnClickListener {
 
 
-}
+
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return kategoriListe.size
+    }
 
 }
--- app/src/main/java/com/asimodabas/movie_detail_app/MainActivity.kt
+++ app/src/main/java/com/asimodabas/movie_detail_app/MainActivity.kt
@@ -2,10 +2,32 @@ package com.asimodabas.movie_detail_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var kategoriListe:ArrayList<Kategoriler>
+    private lateinit var adapter:KategoriAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbarKategori.title = "Kategoriler"
+        setSupportActionBar(toolbarKategori)
+
+        KategoriRv.setHasFixedSize(true)
+        KategoriRv.layoutManager=LinearLayoutManager(this)
+
+        kategoriListe=ArrayList()
+        val k1 = Kategoriler(1,"Komedi")
+        val k2 = Kategoriler(2,"Bilim Kurgu")
+
+        kategoriListe.add(k1)
+        kategoriListe.add(k2)
+
+        adapter=KategoriAdapter(this,kategoriListe)
     }
 }
