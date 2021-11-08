be5c3849bfbd524ab99d9db4bff806f7b05730ad
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-11-09 01:57:24 +0300

    Staggered Grid card deisgn added

--- app/src/main/java/com/asimodabas/movie_detail_app/FilmlerActivity.kt
+++ app/src/main/java/com/asimodabas/movie_detail_app/FilmlerActivity.kt
@@ -2,10 +2,23 @@ package com.asimodabas.movie_detail_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import kotlinx.android.synthetic.main.activity_filmler.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class FilmlerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_filmler)
+
+
+        toolbarFilmler.title = "Filmler"
+        setSupportActionBar(toolbarFilmler)
+
+        filmlerRv.setHasFixedSize(true)
+        filmlerRv.layoutManager = StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
+
+
     }
 }
